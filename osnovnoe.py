2022c54c00207bb51407ce317b1da99c7b843628
Klammi <31730606+Klammi@users.noreply.github.com>
2019-03-27 10:28:32 +0300

    2

--- osnovnoe.py
+++ osnovnoe.py
@@ -29,6 +29,10 @@ def add_message():
 def registration():
     return render_template('registration.html')
 
+@app.route('/signin', methods=['GET'])
+def signin():
+    return render_template('Signin.html')
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=3000)
